8da95f4689c1f14c9cad8f2bd8853ee001d3cf15
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-05-15 10:38:52 +0200

    init project with structure

---
+++ src/container/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    state = {
+        teams: [
+            {id: 1, name: 'Juventus'},
+            {id: 2, name: 'Barcelona'},
+            {id: 3, name: 'Manchester City'},
+            {id: 4, name: 'Bayern'},
+        ]
+    }
+    render() {
+        return (
+            null
+        )
+    }
+}
+
+export default App;
